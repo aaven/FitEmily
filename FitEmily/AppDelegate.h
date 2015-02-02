f52f8e73fa83a7068653c5dcab0291dbb9f19422
Aaven Jin <aavenj@gmail.com>
2015-02-02 23:40:37 +0800

    Initial Commit

---
+++ FitEmily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FitEmily
+//
+//  Created by Aaven Jin on 2/2/15.
+//  Copyright (c) 2015 FE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
